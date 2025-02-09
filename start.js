45a431bb7348359c83caa534edf814d23bbd4ab2
salight <light.sara@icloud.com>
2025-02-09 15:53:29 +0100

    minor fetching issues sorted out

--- start.js
+++ start.js
@@ -4,89 +4,38 @@
 //Utilizing local storage to create, read, update, and delete data.
 
 
-//1. Settings for the API
 
-//use strict;
-//use warnings;
-//use HTTP::Tiny;
-//use Time::HiRes qw/sleep/;
-//use JSON qw/decode_json/;
-
-
-//Modeling this script from Maja's excellent code along videos as a first step.
-//Second step is to make it my own by adding my own code and ideas.
-
-
-console.log("Starting out");
+//console.log("Starting out");
 const BASE_URL = "https://rest.ensembl.org/";
-const ext = "/lookup/id/ENSG00000157764?content-type=application/json;expand=1";
+const ext = "/lookup/id/ENSG00000157764?content-type=application/json;expand=1";  //expand not available for proteins
 const ext1 = "/lookup/id/";
-const ext2 = "?content-type=application/json;expand=1"
-
-
+const ext2 = "?content-type=application/json;"
 
 //Example protein "id": "ENSP00000419060"
 
 //Protein stable ID starts with ENSP
 //Define the protein stable ID's as an array.
 const proteinStableId_array = ["ENSP00000518625", "ENSP00000518623", "ENSP00000518638", "ENSP00000518637", "ENSP00000518635", "ENSP00000518633", "ENSP00000518632", "ENSP00000518590", "ENSP00000518612", "ENSP00000518609"];
-console.log(proteinStableId_array);
 
 
-async function fetchData(protein) {
-    
-    //create the specific URL for each proteinStableId
-    proteinStableId_array.forEach((protein) => {
-    
-    
-            const kombo = BASE_URL + ext1 + protein + ext2;
-            console.log(kombo); 
+  ////create the specific URL for each proteinStableId
+  //proteinStableId_array.forEach((protein) => {}
 
-         //   try {
-         //         const response = await fetch(kombo).json;
-         //       if (!response.ok) {
-         //             throw new Error(`Error status: ${response.status}`);
-          //          }
-          //      } catch (error) {
-                    // console.log(response);
-          //          console.error(error);
-          //XS      }
+//add modified proteinStableId_array to copyItems array
+const urls = [];
+proteinStableId_array.forEach((protein) => {
+  url = BASE_URL + ext1 + protein + ext2;
+  urls.push(url);
+});
 
-        })
-}
+//console.log(urls);
 
-  
-//    try {
- //           console.log(kombo);
- //           const response = await fetch(kombo);
- //           if (!response.ok) {
- //               throw new Error(`Error status: ${response.status}`);
-//
-//            }
+//2. Fetching the data from the API
+//response cors - is cross origin resource sharing
+Promise.allSettled(urls.map(url =>
+  fetch(url).then(texts => {
+  //console.log(`Received request result..., ${texts.json}`)
+  console.log(texts.json())
+  })
 
-//          const data = await response.json();
-//          console.log(data);
-
-//        } catch (error) {
-//        // console.log(response);
-//        console.error(error);
-//
-//        }
-
- //   })
-
-    
-fetchData(proteinStableId_array);
-
-
-//Make changes to each gene object.
-
-
-
-
-
-//I am interested in mapping protein to chromosomal location.
-//https://rest.ensembl.org/documentation/info/assembly_translation
-//This is something I want to try later, so leaving this here for now.
-
-
+))
